91d3fa1d43d22d4a1ccb09eeec845baacc7b9987
webbedfin <potifer@gmail.com>
2016-01-17 01:27:53 -0800

    extract loop sample count:

--- loop_len_shift.py
+++ loop_len_shift.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python
 __copyright__ = """
-                        2016 by Chris Derry.
-                           All rights reserved.
+                        2016 by Chris Derry
 """
 
 import os
 import sys
 import argparse
 import subprocess
+import re
 
 def loop_len_shift(path, offset):
 
     for dirName, subdirList, fileList in os.walk(path, topdown=False):
+
         if dirName.endswith('Media'):
             fileList = [fi for fi in fileList if fi.endswith('.aiff')]
             print('Found directory: %s' % dirName)
             for fname in fileList:
                 print('\t%s' % fname)
                 #subprocess.call(["sox", fname, "trim"])
-                soxi = subprocess.check_output(['sox', '--info', fname], shell=True)
+                soxi = subprocess.check_output(['sox', '--info', dirName + '\\' + fname], shell=True)
                 print soxi
 
-                soxi_split = sox1.split()
+                # find sample length 
+                for line in soxi.split('\n'):
+                    if re.search(r'Dur', line):
+                        for section in line.split('='):
+                            if re.search(r'samples',section):
+                                loop_len = section.split()[0]
 
-                #subprocess.call(['sox', fname, 'temp%1n.aiff', 'trim 0', end-offset, ': newfile : trim 0', offset])
+
+                subprocess.call(['sox', fname, 'temp%1n.aiff', 'trim 0', loop_len-offset, ': newfile : trim 0', offset])
                 #subprocess.call(['sox', 'temp0.aiff', 'temp1.aiff', 'fname_new.aiff'])
 
                 # cleanup
@@ -40,7 +47,6 @@ if __name__ == "__main__":
 
     print """
                     Loop length shift
-
     {0}
 
     """.format(__copyright__)
@@ -56,4 +62,4 @@ if __name__ == "__main__":
         print 'Path does not exist'
         sys.exit(0)
 
-        loop_len_shift(args.path, args.offset)
+    loop_len_shift(args.path, args.offset)
